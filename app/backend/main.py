acd62784b1ecb3f39502183127fafafcdcb6e6e8
Bhupesh Bait <bhupeshbait14@gmail.com>
2026-06-02 11:09:14 +0530

    Added mount function to serve static files to users

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,9 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from api.api import router as api_router
 
 app = FastAPI()
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 app.include_router(api_router)
 
