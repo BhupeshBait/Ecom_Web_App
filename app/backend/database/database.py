7477e69acf92d90bcd667d033472b44ac7629bfa
Bhupesh Bait <bhupeshbait14@gmail.com>
2026-06-13 10:29:55 +0530

    used env vairiables

--- app/backend/database/database.py
+++ app/backend/database/database.py
@@ -2,11 +2,14 @@ import pymysql
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from core.config import DATABASE_URL
 
-url = "mysql+pymysql://root:@localhost:3306/ecomdb"
+
+url = DATABASE_URL 
 
 engine = create_engine(
     url,
-    pool_pre_ping=True,pool_recycle=3600)
+    pool_pre_ping=True, pool_recycle=3600
+)
 local_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 base = declarative_base()
